c1554c585138f17e93c44e75efe8172bb49e55a9
Gijs Molenaar <gijsmolenaar@gmail.com>
2010-02-25 13:54:51 +0100

    got hog working. sort of

--- code/cpp/finder/finder.cpp
+++ code/cpp/finder/finder.cpp
@@ -13,12 +13,130 @@
 #include "cvtypes.h"
 #include "highgui.h"
 #include "cvaux.h"
+#include "finder.h"
 #include <iostream>
+#include <exception>
 
 using namespace cv;
 using namespace std;
 
-Rect face_region(Rect face) {
+
+
+
+
+
+
+float sum(const vector<float>& x) {
+    float total = 0.0;
+    for (unsigned int i=0; i<x.size(); i++)
+        total = total + x[i]; 
+    return total;
+}
+
+
+struct Hand {
+public:
+    Mat img, hsv, backproj, cutout;
+	HOGDescriptor hog;
+	vector<float> descriptors;
+    Hand(const string& filename, MatND histogram);
+    void load_image(const string& filename);
+    void make_hsv();
+    void make_backproject(MatND histogram);
+    void make_cutout();
+	void find_hog();
+};
+
+Hand::Hand(const string& filename, MatND histogram) {
+    load_image(filename);
+    make_backproject(histogram);
+    make_cutout();
+	find_hog();
+}
+
+void Hand::load_image(const string& filename) {
+    img = imread(filename, 1);
+    if (!img.data) {
+        throw exception();
+	}
+    cvtColor(img, hsv, CV_BGR2HSV);
+};
+
+void Hand::make_backproject(MatND histogram) {
+    const float hranges[] = { 0, 180 };
+    const float sranges[] = { 0, 256 };
+    const float* ranges[] = { hranges, sranges };
+    int channels[] = {0, 1};
+    calcBackProject( &hsv, 1, channels, histogram, backproj, ranges );
+}
+
+void Hand::make_cutout() {
+    Mat mask, clean, sized, sub, bw;
+    GaussianBlur( backproj, mask, Size(51, 51), 0);
+    threshold(mask, mask, 80, 1, CV_THRESH_BINARY);
+
+    vector<vector<Point> > contours;
+    findContours( mask, contours, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE );
+    //drawContours( working, contours, -1, Scalar( 0, 0, 255 ));
+
+    Rect box = boundingRect(contours.at(0));
+    //rectangle(mask, box.tl(), box.br(), Scalar(0, 255, 0) );
+
+    img.copyTo(clean, mask);
+    sub = clean(box);
+    resize(sub, sized, Size(64,128));
+    cvtColor(sized, bw, CV_BGR2GRAY);
+    equalizeHist(bw, bw);
+    bw.copyTo(cutout);
+}
+
+void Hand::find_hog() {
+	hog = HOGDescriptor();
+	vector<Point> locations;
+	Size winStride = Size(8, 8);
+	Size padding = Size(0, 0);
+	hog.compute(cutout, descriptors, winStride, padding, locations);
+};
+
+struct Skin {
+public:
+    Mat img, hsv, bw, facepixels;
+    MatND histogram;
+	vector<Rect> faces;
+    CascadeClassifier haarzoeker;    
+    Skin(const string& facefile, const string& haarfile);
+    void load_image(const string& filename);
+    void load_haar(const string& filename);
+    void find_face();
+    void make_histogram();
+    Rect face_region(Rect face);
+};
+
+Skin::Skin(const string& facefile, const string& haarfile) {
+    load_image(facefile);
+	load_haar(haarfile);
+    find_face();
+    make_histogram();
+}
+
+void Skin::load_image(const string& filename) {
+    img = imread(filename, 1);
+    if (!img.data) {
+		cout << "can't load" << filename << endl;
+        throw exception();
+	}
+    cvtColor(img, hsv, CV_BGR2HSV);
+    cvtColor(img, bw, CV_BGR2GRAY);
+};
+
+void Skin::load_haar(const string& filename) {
+	if ( !haarzoeker.load(FACEHAAR) ) {
+		cerr << "can't load" << filename << endl;
+		throw exception();
+    }
+}
+
+Rect Skin::face_region(Rect face) {
     Rect r;
     r.x = face.x + face.width * 0.2;
     r.y = face.y + face.height * 0.15;
@@ -27,146 +145,66 @@ Rect face_region(Rect face) {
     return r;
 }
 
-    //VideoCapture cap(0);
-    //if(!cap.isOpened()) {
-        //cout << "couldn't open video\n";
-        //return -1;
-    //}
-
-
-float sum(const vector<float>& x) {
-    float total = 0.0;  // the sum is accumulated here
-    for (unsigned int i=0; i<x.size(); i++) {
-        total = total + x[i]; 
-		cout << "x: " << x[i] << endl;
-		cout << "xsize: " << x.size() << endl;
-		cout << "i: " << i << endl;
-    }
-    return total;
+void Skin::find_face() {
+    haarzoeker.detectMultiScale(img, faces, 1.1, 2, CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+	if (faces.size() == 0) {
+		cerr << "no faces found in image" << endl;
+		throw exception();
+	}
+	Rect face = faces.at(0);
+    Rect region = face_region(face);
+    facepixels = hsv(region);
 }
 
-int main(int, char**) {
-
-    //setNumThreads(5);
-
-    CascadeClassifier haarzoeker;
-	Mat hsv_skin, hsv_head, result, bw_head, hsv_handa, bp;
-    Mat skin = imread(SKIN, 1);
-	if (!skin.data) {
-	 cerr << "can't load skin" << endl;
-		return -1;
-	}
-    Mat head = imread(HEAD, 1);
-	if (!head.data) {
-		cerr << "can't load head" << endl;
-		return -1;
-	}
-	Mat handa = imread(HANDA, 1);
-	if (!handa.data) {
-		cerr << "can't load handa" << endl;
-		return -1;
-	}
-
-    if ( !haarzoeker.load(FACEHAAR) ) {
-        cerr << "haar werkt niet" << endl;
-    };
-
-
-    cvtColor(skin, hsv_skin, CV_BGR2HSV);
-    cvtColor(head, hsv_head, CV_BGR2HSV);
-    cvtColor(handa, hsv_handa, CV_BGR2HSV);
-    cvtColor(head, bw_head, CV_BGR2GRAY);
-
-    vector<Rect> faces;
-    haarzoeker.detectMultiScale(bw_head, faces, 1.1, 2, CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-    Rect face = faces[0];
-    Rect region = face_region(face);
-    Mat sub_face = hsv_head(region);
-    //rectangle(head, region, CV_RGB(0,0,255), 1, 1);
-    MatND hist;
-
+void Skin::make_histogram() {
     int hbins = 30, sbins = 32;
     int histSize[] = {hbins, sbins};
     const float hranges[] = { 0, 180 };
     const float sranges[] = { 0, 256 };
     const float* ranges[] = { hranges, sranges };
     int channels[] = {0, 1};
-    calcHist( &sub_face,  1, channels, Mat(), hist, 2,  histSize, ranges );
+    calcHist( &facepixels,  1, channels, Mat(), histogram, 2,  histSize, ranges );
+}
 
-    calcBackProject( &hsv_handa, 1, channels, hist, bp, ranges );
-    GaussianBlur( bp, bp, Size(51, 51), 0);
-    threshold(bp, result, 80, 1, CV_THRESH_BINARY);
+int main(int, char**) {
 
+    //setNumThreads(5);
 
-    vector<vector<Point> > contours;
-    findContours( result, contours, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE );
-    drawContours( handa, contours, -1, Scalar( 0, 0, 255 ));
-
-    Rect box = boundingRect(contours.at(0));
-    rectangle(handa, Point(box.x, box.y), Point(box.x+box.width, box.y+box.height), Scalar(0, 255, 0) );
-
-    Mat clean;
-    handa.copyTo(clean, result);
-    Mat sub = clean(box);
-
-    Mat sized;
-    CvSize window = Size(64,128);
-    resize(sub, sized, window);
+    Skin skin(HEAD, FACEHAAR);
+	Hand handa(HANDA, skin.histogram);
+	Hand handb(HANDB, skin.histogram);
+	cout << handa.descriptors.size() << endl;
+	cout << handb.descriptors.size() << endl;
 	
-	Mat sized_bw;
-	cvtColor(sized, sized_bw, CV_BGR2GRAY);
-	equalizeHist(sized_bw, sized_bw);
 	
-    HOGDescriptor h = HOGDescriptor();
-    vector<float> descriptors;
-    vector<Point> locations;
-	Size winStride = Size(64, 64);
-	Size padding = Size(0, 0);
-    h.compute(sized_bw, descriptors, winStride, padding, locations);
-	cout << descriptors.size() << endl;
 	
-	//cout << "sum: "	<< sum( descriptors );
 
-	//imshow("clean", sized);
-    //imshow( "sub_face", handa);
-    //imshow( "head", head);
-    //imshow( "backproject", result );
-    imshow( "sized", sized_bw );
-
-	waitKey();
+//	VideoCapture cap(0);
+//    if(!cap.isOpened()) {
+//	  cout << "couldn't open video\n";
+//	  return -1;
+//    }
+//	
+//	Mat frame;
+//	for(;;)
+//    {
+//		CascadeClassifier haarzoeker;
+//        cap >> frame;
+//		vector<Rect> faces;
+//		Rect face;
+//		haarzoeker.detectMultiScale(frame, faces, 1.1, 2, CV_HAAR_SCALE_IMAGE, Size(10, 10) );
+//		//cout << faces.size() << endl;
+//		for (int i = 0; i <faces.size(); i++) {
+//			face = faces[i];
+//			cout << "ja!" << endl;
+//			rectangle(frame, face.tl(), face.br(), Scalar(0, 255, 0));
+//        } 
+//        imshow("edges", frame);
+//        if(waitKey(30) >= 0)
+//			break;
+//    }
+//	
 	
-	return 0;
-	//cout << descriptors.
-
-    //IplImage** integrals;
-    //IplImage img = IplImage(sized);
-	//IplImage img = sized;
-
-
-    //integrals = calculateIntegralHOG(&img);
-
-	//int normalization = 4;
-    //CvMat* img_feature_vector;
-    //img_feature_vector = calculateHOG_window(integrals, cvRect(0, 0, window.width, window.height), normalization);
-
-
-    //cvShowImage("henk", &img);
-    //waitKey();
-
-
-    /* Mat edges;
-    namedWindow("edges",1);
-    for(;;)
-    {
-        Mat frame;
-        cap >> frame;
-        cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
-    }
-	 */
 
 
 }
