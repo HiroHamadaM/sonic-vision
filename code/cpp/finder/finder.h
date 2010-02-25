8544cd50697053d03a6e516000a542be995fd83b
Gijs Molenaar <gijsmolenaar@gmail.com>
2010-02-25 14:04:24 +0100

    forgot this one

---
+++ code/cpp/finder/finder.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   finder.h
+ * Author: gijs
+ *
+ * Created on February 24, 2010, 2:20 PM
+ */
+
+#ifndef _FINDER_H
+#define	_FINDER_H
+
+#include "cv.h"
+
+using namespace cv;
+
+
+
+#endif	/* _FINDER_H */
+
