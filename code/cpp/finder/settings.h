b5f8a925665ebf92e283a75ef2bf6a14e11156f7
Gijs Molenaar <gijsmolenaar@gmail.com>
2010-03-04 16:56:29 +0100

    moved hog

--- code/cpp/finder/settings.h
+++ code/cpp/finder/settings.h
@@ -10,11 +10,13 @@
 #define HANDD "../../../data/hand/d.png"
 #define FACEHAAR "/usr/local/share/opencv/haarcascades/haarcascade_frontalface_alt.xml"
 
-//#define DEVICE "../../../data/movies/heiligenacht.mp4"
-#define DEVICE 0
+#define DEVICE "../../../data/movies/heiligenacht.mp4"
+//#define DEVICE 0
 //#define DEVICE "../../../data/movies/wayne_cotter.mp4"
 
 #define WORKSIZE 300
 #define XWINDOWS 3
+
+#define GUIDONIAN_PATH "../../../data/guidonian"
 
 #endif
