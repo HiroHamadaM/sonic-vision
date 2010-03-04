5620714e11d6a10ef4d3b5783df8bf990d63f59d
Gijs Molenaar <gijsmolenaar@gmail.com>
2010-03-04 16:29:18 +0100

    added new training data

--- code/cpp/finder/tools.cpp
+++ code/cpp/finder/tools.cpp
@@ -17,10 +17,10 @@ float sum(const vector<float>& x) {
 
 Rect sub_region(Rect region) {
     Rect r;
-    r.x = region.x + region.width * 0.2;
-    r.y = region.y + region.height * 0.15;
-    r.width = region.width * 0.6;
-    r.height = region.height * 0.7;
+    r.x = region.x + region.width * 0.3;
+    r.y = region.y + region.height * 0.2;
+    r.width = region.width * 0.4;
+    r.height = region.height * 0.6;
     return r;
 }
 
